4955fd445ce89e5b14c4e8f42818f532c9427410
Philippe Chataignon <philippe.chataignon@gmail.com>
2021-12-03 15:34:03 +0100

    Another ram tester memtest.s

---
+++ memtest.s
@@ -0,0 +1,48 @@
+START       equ $FA
+END         equ $FB
+CURL        equ $FC
+CURH        equ $FD
+COUT1       equ $FDF0
+CR          equ $FC62
+
+            org $804
+INIT
+            ldy #0
+            sty CURL
+            lda START
+            sta CURH
+
+LOOP1
+            lda     (CURL),y    ; load address
+            eor     #$ff        ; inverse bits
+            sta     (CURL),y    ; and store
+            cmp     (CURL),y    ; cmp memory location
+            bne     ERROR       ; if different, error
+            eor     #$ff        ; restore original value
+            sta     (CURL),y    ; and store
+            iny                 ; next byte
+            bne     LOOP1       ;
+            inc     CURH        ; next page
+            lda     CURH
+            cmp     END         ; last page ?
+            bcc     LOOP1       ; no, continue
+OK            
+            lda    #'P'+$80
+            jsr    COUT1
+            lda    #'A'+$80
+            jsr    COUT1
+            lda    #'S'+$80
+            jsr    COUT1
+            lda    #'S'+$80
+            jsr    COUT1
+            jsr    CR
+            rts
+ERROR            
+            lda    #'E'+$80
+            jsr    COUT1
+            lda    #'R'+$80
+            jsr    COUT1
+            lda    #'R'+$80
+            jsr    COUT1
+            jsr    CR
+            rts
