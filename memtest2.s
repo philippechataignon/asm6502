63a49c391c515e10cc627c9e2e9a7a7a395c8091
Philippe Chataignon <philippe.chataignon@gmail.com>
2021-12-05 21:19:23 +0100

    memtest2 displays write/read page

--- memtest2.s
+++ memtest2.s
@@ -13,60 +13,94 @@ START       equ $FA
 END         equ $FB
 CURL        equ $FC
 CURH        equ $FD
+SCREEN      equ $400            ; line 0
 COUT1       equ $FDF0
 CR          equ $FC62
 HOME        equ $FC58
 
-            org $2A0
+            org $280
 INIT
+            jsr     HOME
             ldy #0
             sty CURL
             lda START
             sta CURH
 
 LOOP1
+            cpy     #0
+            bne     .NOBYTEOUT
+            lda     CURH
+            jsr     BYTEOUT
+.NOBYTEOUT
             tya
             sta     (CURL),y    ; and store
             iny                 ; next byte
-            bne     LOOP1       ;
+            bne     LOOP1      
             inc     CURH        ; next page
             lda     CURH
             cmp     END         ; last page ?
-            bcc     LOOP1       ; no, continue
+            bcc     LOOP1      ; no, continue
 
             lda START
             sta CURH
 
 LOOP2
+            cpy     #0
+            bne     .NOBYTEOUT
+            lda     CURH
+            jsr     BYTEOUT
+.NOBYTEOUT
             tya
             cmp     (CURL),y    ; cmp y and addr
             bne     ERROR
             iny                 ; next byte
-            bne     LOOP2       ;
+            bne     LOOP2     
             inc     CURH        ; next page
             lda     CURH
             cmp     END         ; last page ?
-            bcc     LOOP2       ; no, continue
+            bcc     LOOP2      ; no, continue
 
 OK            
-            jsr     HOME
-            lda     #'P'+$80
-            jsr     COUT1
-            lda     #'A'+$80
-            jsr     COUT1
-            lda     #'S'+$80
-            jsr     COUT1
-            lda     #'S'+$80
-            jsr     COUT1
-            jsr     CR
+            ldx     #LEN_PASS
+.MSG
+            lda     MSG_PASS-1,X
+            sta     SCREEN,X
+            dex
+            bne     .MSG
             rts
 ERROR            
-            jsr     HOME
-            lda     #'E'+$80
-            jsr     COUT1
-            lda     #'R'+$80
-            jsr     COUT1
-            lda     #'R'+$80
-            jsr     COUT1
-            jsr     CR
+            ldx     #LEN_ERR
+.MSG
+            lda     MSG_ERR-1,X
+            sta     SCREEN,X
+            dex
+            bne     .MSG
             rts
+
+BYTEOUT
+            pha                 ; push A for processing high nibble
+            lsr                 ; 
+            lsr
+            lsr
+            lsr
+            ora     #$F0        
+            cmp     #$FA
+            bcc     .WRT1
+            sbc     #$39
+
+.WRT1
+            sta     SCREEN + 38
+            pla
+            and     #$0F
+            ora     #$F0
+            cmp     #$FA
+            bcc     .WRT2
+            sbc     #$39
+.WRT2
+            sta     SCREEN + 39
+            rts
+
+MSG_PASS    abyte   (0b00111111 & ._),"PASS"  ; set bit 7 = 1
+LEN_PASS    equ     * - MSG_PASS
+MSG_ERR     abyte   (0b01111111 & ._),"ERR"
+LEN_ERR     equ     * - MSG_ERR
