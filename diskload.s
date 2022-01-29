d404c740137152a017fa91595b8fe93c88906870
Philippe Chataignon <philippe.chataignon@gmail.com>
2022-01-29 15:41:57 +0100

    Loops over trk/sec

--- diskload.s
+++ diskload.s
@@ -1,57 +1,66 @@
-;diskload2.s
+            ifndef ORG
+ORG         =   $9000
+            endif
+
+            if ORG > 0
+            org     ORG
+            fi
+
+load8000    =   $280
 
 ; apple vectors
 
-dos         =    $9D84
-asrom       =    $9D72
-init        =    $A54F
-tapein      =    $C060        ; read tape interface
-motoroff    =    $C088        ; Turn drive motor off
-motoron     =    $C089        ; Turn drive motor on
-reboot      =    $FAA6        ; reboot machine
-tabv        =    $FB5B        ; move cursor to ch,a
-bascalc     =    $FBC1        ; calc line addr
-bell        =    $FBDD        ; ding
-cleos       =    $FC42        ; clear to end of screen
-clear       =    $FC58        ; clear screen
-rdkey       =    $FD0C        ; read key
-crout       =    $FD8E        ; CR out sub
-prbyte      =    $FDDA         ; print byte in hex
-cout        =    $FDED        ; character out sub
-warm        =    $FF69        ; back to monitor
+dos         =   $9D84
+asrom       =   $9D72
+init        =   $A54F
+tapein      =   $C060        ; read tape interface
+motoroff    =   $C088        ; Turn drive motor off
+motoron     =   $C089        ; Turn drive motor on
+reboot      =   $FAA6        ; reboot machine
+tabv        =   $FB5B        ; move cursor to ch,a
+bascalc     =   $FBC1        ; calc line addr
+bell        =   $FBDD        ; ding
+cleos       =   $FC42        ; clear to end of screen
+clear       =   $FC58        ; clear screen
+rdkey       =   $FD0C        ; read key
+crout       =   $FD8E        ; CR out sub
+prbyte      =   $FDDA         ; print byte in hex
+cout        =   $FDED        ; character out sub
+warm        =   $FF69        ; back to monitor
 
 ;            dos routines
 
-fm          =    $3D6        ; file manager entry
-rwts        =    $3D9        ; RWTS jsr
-locfpl      =    $3DC        ; locate file manager paramlist jsr
-locrpl      =    $3E3        ; locate RWTS paramlist jsr
+fm          =   $3D6        ; file manager entry
+rwts        =   $3D9        ; RWTS jsr
+locfpl      =   $3DC        ; locate file manager paramlist jsr
+locrpl      =   $3E3        ; locate RWTS paramlist jsr
 
 ;            zero page parameters
 
-begload     =    $D0        ; begin load location LSB/MSB
-endload     =    $D2        ; end load location LSB/MSB
-chksum      =    $D4        ; checksum location
-secnum      =    $D5        ; loop var
-trknum      =    $D6        ; loop var
-segcnt      =    $D7        ; loop var
-buffer      =    $D8        ; MSB of RWTS buffer
-trkcnt      =    $D9        ; track counter (0-6)
-pointer     =    $DA        ; pointer LSB/MSB
-prtptr      =    $DC        ; pointer LSB/MSB
-fmptr       =    $DE        ; file manager pointer
-temp        =    $E0        ; temp var
+begload     =   $D0        ; begin load location LSB/MSB
+endload     =   $D2        ; end load location LSB/MSB
+chksum      =   $D4        ; checksum location
+secnum      =   $D5        ; loop var
+trknum      =   $D6        ; loop var
+segcnt      =   $D7        ; loop var
+buffer      =   $D8        ; MSB of RWTS buffer
+trkcnt      =   $D9        ; track counter (0-6)
+pointer     =   $DA        ; pointer LSB/MSB
+prtptr      =   $DC        ; pointer LSB/MSB
+fmptr       =   $DE        ; file manager pointer
+temp        =   $E0        ; temp var
 
 ;             monitor vars
 
-ch          =    $24        ; cursor horizontal
-basl        =    $28        ; line addr form bascalc L
-bash        =    $29        ; line addr form bascalc H
-preg        =    $48        ; mon p reg
+ch          =   $24        ; cursor horizontal
+basl        =   $28        ; line addr form bascalc L
+bash        =   $29        ; line addr form bascalc H
+preg        =   $48        ; mon p reg
 
 ;            other vars
 
-;data        =    $1000        ; 7 track loaded
+data        =   $1000         ; 7 track loaded in $1000-$8000
+enddata     =   $8000
 ;boot1o      =    $96D0        ; tape loaded boot 1 location
 ;boot1       =    $3D0         ; target boot 1 location
 ;cmpbuf      =    $9200        ; buffer for sector check
@@ -82,18 +91,6 @@ start:
             lda #<left           ; print left side of grid
             ldy #>left
             jsr print
-            jsr rdkey
-
-            lda #20
-            sta trknum
-            lda #5
-            sta secnum          ;sect 6
-            jsr draw_r
-            jsr rdkey
-            jsr draw_w
-            jsr rdkey
-            jsr draw_s
-            jsr rdkey
 
 ;;setupiob:
 ;;            jsr locrpl           ; locate rwts paramlist
@@ -167,33 +164,28 @@ start:
 ;;formaterror:
 ;;            jmp diskerror
 ;;endformat:
-;;
-;;                                 ;;;begin segment loop (5)
-;;            lda #0               ; 256 bytes/sector
-;;            ldy #$0b             ; offset in RWTS
-;;            sta (pointer),y      ; write it to RWTS
-;;
-;;            lda #0               ; buffer LSB
-;;            ldy #8               ; offset in RWTS
-;;            sta (pointer),y      ; write it to RWTS
-;;
-;;            lda #0
-;;            sta trknum           ; start with track 0
-;;            lda #5
-;;            sta segcnt
+                                 ;;;begin segment loop (5)
+            lda #0               ; 256 bytes/sector
+            ldy #$0b             ; offset in RWTS
+            sta (pointer),y      ; write it to RWTS
+
+            lda #0               ; buffer LSB
+            ldy #8               ; offset in RWTS
+            sta (pointer),y      ; write it to RWTS
+
+            lda #0
+            sta trknum           ; start with track 0
+            lda #5
+            sta segcnt
 segloop:
 
                                  ;;; get 7 tracks from tape
 load:
+            jsr draw_r
             jsr status
             lda #<loadm          ; print loading data
             ldy #>loadm
             jsr print            ; flash
-            jsr rdkey
-            lda #<loadm2         ; print loading data
-            ldy #>loadm2
-            jsr print
-            jsr rdkey
 
             sec
             lda #5
@@ -203,35 +195,21 @@ load:
             tax
             stx temp
 
-            lda infdata+2,x      ; get sec
-            jsr cout
-            lda infdata+3,x      ; get sec
-            beq second
-            jsr cout
-second:
-            lda #<secm           ; print sec
-            ldy #>secm
-            jsr print
-
             ldx temp
-            lda infdata+0,x      ; store begin location LSB
+            lda #$00
             sta begload
-            lda infdata+1,x      ; store begin location MSB
-            sta begload+1
-
-            lda #$00             ; store end location LSB
             sta endload
-            lda #$90             ; store end location MSB
+            lda #>data
+            sta begload+1
+            lda #>enddata         ; store end location MSB
             sta endload+1
 
-;;            jsr readtape         ; get the code
-;;inf:
-;;                                 ; turn motor on to save 1-2 sec
-;;            ldx #$60             ; slot #6
-;;            lda motoron,x        ; turn it on
+            ;jsr load8000         ; get data in $1000-$8000
+                                 ; turn motor on to save 1-2 sec
+            ldx #$60             ; slot #6
+            lda motoron,x        ; turn it on
 
             jsr status
-
                                  ;;;begin track loop (7)
             jsr status
             lda #<writem         ; print writing
@@ -239,59 +217,58 @@ second:
             jsr print
             jsr rdkey
 
-;;            lda #>data
-;;            sta buffer
-;;            lda #7
-;;            sta trkcnt           ; do 7 tracks/segment
-;;trkloop:
-;;            lda trknum           ; track number
-;;            ldy #4               ; offset in RWTS
-;;            sta (pointer),y      ; write it to RWTS
-;;
-;;
-;;                                         ;;;begin sector loop (16), backwards is faster, much faster
-;;            lda #$F
-;;            sta secnum
-;;secloop:
-;;            jsr draw_s           ; write sector from buffer to disk
-;;            lda secnum           ; sector number
-;;            ldy #5               ; offset in RWTS
-;;            sta (pointer),y      ; write it to RWTS
-;;
-;;            lda buffer           ; buffer MSB
-;;            clc
-;;            adc secnum
-;;            ldy #9               ; offset in RWTS
-;;            sta (pointer),y      ; write it to RWTS
-;;
-;;            lda #2               ; read(1)/write(2) command
-;;            ldy #$0C             ; offset in RWTS
-;;            sta (pointer),y      ; write it to RWTS
-;;
-;;            jsr locrpl           ; locate rwts paramlist
-;;            jsr rwts             ; do it!
-;;            bcs diskerror
-;;            lda #0
-;;            sta preg             ; fix p reg so mon is happy
-;;
-;;                                             ;;; compare code
-;;            dec secnum
-;;            bpl secloop
-;;                                             ;;;end sector loop
-;;
-;;            lda buffer           ; buffer += $10
-;;            clc
-;;            adc #$10
-;;            sta buffer
-;;
-;;            inc trknum           ; next track
-;;            dec trkcnt           ;
-;;            bne trkloop          ; 0, all done with 7 tracks
-;;                                         ;;;end track loop
-;;
-;;            dec segcnt
-;;            beq done             ; 0, all done with 5 segments
-;;            jmp segloop
+            lda #>data
+            sta buffer
+            lda #7
+            sta trkcnt           ; do 7 tracks/segment
+trkloop:
+            lda trknum           ; track number
+            ldy #4               ; offset in RWTS
+            sta (pointer),y      ; write it to RWTS
+
+
+                                 ;;;begin sector loop (16), backwards is faster, much faster
+            lda #$F
+            sta secnum
+secloop:
+            jsr draw_w           ; write sector from buffer to disk
+            lda secnum           ; sector number
+            ldy #5               ; offset in RWTS
+            sta (pointer),y      ; write it to RWTS
+
+            lda buffer           ; buffer MSB
+            clc
+            adc secnum
+            ldy #9               ; offset in RWTS
+            sta (pointer),y      ; write it to RWTS
+
+            lda #2               ; read(1)/write(2) command
+            ldy #$c              ; offset in RWTS
+            sta (pointer),y      ; write it to RWTS
+
+            ;jsr locrpl           ; locate rwts paramlist
+            ;jsr rwts             ; do it!
+            ;bcs diskerror
+            jsr draw_s           ; write space
+            lda #0
+            sta preg             ; fix p reg so mon is happy
+
+            dec secnum
+            bpl secloop
+
+            lda buffer           ; buffer += $10
+            clc                  ; next page to write
+            adc #$10
+            sta buffer
+
+            inc trknum           ; next track
+            dec trkcnt           ;
+            bne trkloop          ; 0, all done with 7 tracks
+                                         ;;;end track loop
+
+            dec segcnt
+            beq done             ; 0, all done with 5 segments
+            jmp segloop
                                          ;;;end segment loop
 
                                          ;;; prompt for data only load?
@@ -330,10 +307,10 @@ status:
             jsr tabv          ; move cursor to $24,a; 0 base
             jmp cleos
 draw_w:                          ; print a 'W' in the grid
-            ldx #'W'
+            ldx #'W'-$40
             jmp draw
 draw_r:                          ; print a 'R' in the grid
-            ldx #'R'-$40
+            ldx #'R'+$80
             jmp draw
 draw_s:                          ; print a ' ' in the grid
             ldx #$80+"."
@@ -385,12 +362,6 @@ inflatem:
 loadm:
             abyte   ._ | $40,"LOADING DATA"
             byte    0
-loadm2:
-            abyte    +$80,", ETA "
-            byte    0
-secm:
-            abyte    +$80," SEC. "
-            byte    0
 formatm:
             abyte    +$80,"FORMATTING DISK "
             byte    0
@@ -426,4 +397,3 @@ left:
             abyte    +$80,"  E:",$0D
             abyte    +$80,"  F:",$0D
             byte    0
-infdata:
