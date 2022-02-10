c7c819a9a644088cbc7e695d231505027a615ac5
Philippe Chataignon <philippe.chataignon@gmail.com>
2022-02-10 11:34:10 +0100

    Use .for in div32

--- div32.s
+++ div32.s
@@ -1,5 +1,5 @@
 ORG     :?= $BC80
-SIZE    = 4
+BYTE    = 4
 
 ; N and R must be adjacent
 ; Zero-Page   ; INPUT   ; OUPUT
@@ -13,43 +13,37 @@ R       :?= $FC ; 0       ; r = q - (n/d)*d
         .fi
 
         ; init R = 0
-        ldx #SIZE-1
         ldy #0
-_L1     sty R,X
-        dex
-        bpl _L1
+.for i in range(BYTE)
+        sty R+i
+.next
 
         ; main loop: 32 iterations
-        ldx #8*SIZE
-LOOP    asl N+0         ; Shift high bit of N into R
-        rol N+1         ; N LSB = 0 (ASL) and will be 1
-        rol N+2         ; if INC N below
-        rol N+3         ; N high bit enters in R low
-        rol R+0
-        rol R+1
-        rol R+2
-        rol R+3
+        ldx #8*BYTE
+        ; Shift high bit of N into R
+        ; N LSB = 0 (clc) and will be 1
+        ; if INC N below               
+        ; N high bit enters in R low   
+LOOP    clc
+.for i in range(2*BYTE)
+        rol N+i
+.next
+
+        ; T = R - D on 32 bits
         sec
-        lda R+0         ; T = R - D on 32 bits
-        sbc D+0
-        sta T+0
-        lda R+1
-        sbc D+1
-        sta T+1
-        lda R+2
-        sbc D+2
-        sta T+2
-        lda R+3
-        sbc D+3
-        sta T+3
+.for i in range(BYTE)
+        lda R+i         
+        sbc D+i
+        sta T+i
+.next
         blt NEXT        ; R < D -> next
         inc N           ; N LSB 0->1
+
         ; Update R <- T
-        ldy #SIZE-1
-_L2     lda T,Y
-        sta R,Y
-        dey
-        bpl _L2
+.for i in range(BYTE)
+        lda T+i
+        sta R+i
+.next
 NEXT    dex        ; next bit
         bne LOOP   ; if any
         rts
