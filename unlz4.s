77961f92497a7ae46be4d7e47b5babaedaa63acc
Philippe Chataignon <philippe.chataignon@gmail.com>
2022-02-10 19:03:33 +0100

    Add comments

--- unlz4.s
+++ unlz4.s
@@ -9,7 +9,7 @@ src =   $FA
 end =   $FC
 dst =   $FE
 count = $CE
-delta = $D6
+offset = $D6
 
 DEST =  $6000
 
@@ -26,7 +26,8 @@ init
         sta dst
         lda #>DEST
         sta dst+1
-unpack ;unpacker entrypoint
+
+unpack
         ldy #0          ; Y is always 0
 parsetoken
         jsr getsrc      ; get token
@@ -39,49 +40,49 @@ parsetoken
         jsr buildcount
         tax
         jsr docopy
-        lda src
+        lda src         ; src >= end ?
         cmp end
         lda src+1
         sbc end+1
-        bcs done
+        bge done        ; yes, done
 
-copymatches
+copymatches             ; else copymatch phase
+        jsr getsrc      ; get offset (2 bytes)
+        sta offset
         jsr getsrc
-        sta delta
-        jsr getsrc
-        sta delta+1
-        pla
-        and #$0f
+        sta offset+1
+        pla             ; get token
+        and #$0f        ;
         jsr buildcount
-        clc
+        clc             ; add 4 to get matchlength
         adc #4
-        tax
-        bcc +
-        inc count+1
+        tax             ;
+        bcc +           ; if more than $FF
+        inc count+1     ; inc count+1
 +       lda src+1
         pha
         lda src
-        pha
+        pha             ; push src on stack
         sec
         lda dst
-        sbc delta
+        sbc offset      ; src = dst - offset
         sta src
         lda dst+1
-        sbc delta+1
+        sbc offset+1
         sta src+1
-        jsr docopy
-        pla
+        jsr docopy      ;
+        pla             ; restore src
         sta src
         pla
         sta src+1
-        jmp parsetoken
+        jmp parsetoken  ; end of block -> next token
 
 done
-        pla
+        pla             ; restore stack (parsetoken pha)
         rts
 
-docopy
-        jsr getput
+docopy                  ; copy X + 256 *  [count+1] litterals
+        jsr getput      ; get value
         dex
         bne docopy
         dec count+1
@@ -93,28 +94,28 @@ buildcount              ; build count from token nibble
         stx count+1
         cmp #$0f        ; if == $f
         bne _no_f
--       sta count       ; store in LSB count
+-       sta count       ; store last count in LSB count
         jsr getsrc      ; get next byte
         tax
         clc
-        adc count       ; and add to count
+        adc count       ; add it to count
         bcc +
         inc count+1
-+       inx
++       inx             ; if $FF read, tax and inx => X=0 -> continue
         beq -
 _no_f   rts
 
 getput
-        jsr getsrc
+        jsr getsrc      ; (dst) <-  (src)
 
-putdst
+putdst                  ; store to (dst++)
         sta (dst), y
         inc dst
         bne +
         inc dst+1
 +       rts
 
-getsrc
+getsrc                  ; get from (src++)
         lda (src), y
         inc src
         bne +
@@ -124,4 +125,3 @@ getsrc
 pakoff
 .binary "integer.s.lz4"
 paksize = * - pakoff - 1
-
