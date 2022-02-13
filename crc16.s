b0a9a67abe2f2d7229eaaa69b7145f785ebe26f1
Philippe Chataignon <philippe.chataignon@gmail.com>
2022-02-13 17:20:38 +0100

    Add crc16.s

---
+++ crc16.s
@@ -0,0 +1,66 @@
+ptr = $ce
+prbyte = $fdda
+
+* = $803
+
+        jmp ENTRY
+
+START   .word 1
+END     .word 2
+
+ENTRY   ldy start
+        lda start+1
+        sta ptr+1
+        ldx #$FF
+        stx crcl
+        stx crch
+        inx             ; X = 0
+        stx ptr
+loop    lda (ptr),Y
+crc16_f eor crch        ; A contained the data
+        sta crch        ; XOR it into high byte
+        lsr             ; right shift A 4 bits
+        lsr             ; to make top of x^12 term
+        lsr             ; ($1...)
+        lsr
+        tax             ; save it
+        asl             ; then make top of x^5 term
+        eor crcl        ; and XOR that with low byte
+        sta crcl        ; and save
+        txa             ; restore partial term
+        eor crch        ; and update high byte
+        sta crch        ; and save
+        asl             ; left shift three
+        asl             ; the rest of the terms
+        asl             ; have feedback from x^12
+        tax             ; save bottom of x^12
+        asl             ; left shift two more
+        asl             ; watch the carry flag
+        eor crch        ; bottom of x^5 ($..2.)
+        sta crch        ; save high byte
+        txa             ; fetch temp value
+        rol             ; bottom of x^12, middle of x^5!
+        eor crcl        ; finally update low byte
+        ldx crch        ; then swap high and low bytes
+        sta crch
+        stx crcl        ; end CRC16_F
+        iny             ; next byte
+        bne +
+        inc ptr+1
+        beq ++          ; if 0 after $ffff, exit
++       sty tmp
+        lda end
+        cmp tmp
+        lda end+1
+        sbc ptr+1
+        bge loop        ; end >= cur, continue
++       lda crch
+        jsr prbyte
+        lda crcl
+        jsr prbyte
+        rts
+
+crcl    .byte ?         ; current value of CRC
+crch    .byte ?         ; not necessarily contiguous
+tmp     .byte ?
+
