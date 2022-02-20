9261755292a804dbf07259f4893309ac6392ec1d
Philippe Chataignon <philippe.chataignon@gmail.com>
2022-02-20 23:58:56 +0100

    Get param after jsr

---
+++ inline_param.s
@@ -0,0 +1,31 @@
+param = $bf00
+paramsize = 6
+src = param
+end = param + 2
+dst = param + 4
+ptr = $ce
+
+.if DIRECT
+* = $300
+.fi
+
+
+inline      pla              ; get calling addr + 2 in ptr
+            sta ptr
+            pla
+            sta ptr+1
+            ldx #0
+            ldy #0           ; always 0
+-           inc ptr
+            bne +
+            inc ptr+1
++           lda (ptr),Y      ; get param
+            sta param,X
+            inx
+            cpx #paramsize
+            bne -
+            lda ptr+1        ; use ptr as return addr
+            pha
+            lda ptr
+            pha
+            rts
