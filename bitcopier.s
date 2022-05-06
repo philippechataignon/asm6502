0d01dfc2dfaa5bb676910ab9e56a854bf9d597e9
Philippe Chataignon <philippe.chataignon@gmail.com>
2022-05-06 23:17:16 +0200

    Align bitcopier

--- bitcopier.s
+++ bitcopier.s
@@ -27,7 +27,7 @@ SCREEN = $0583
 
 
 * = $2B00
- JMP START
+            JMP START
 
 ;***********************
 ;                      *
@@ -36,19 +36,19 @@ SCREEN = $0583
 ;***********************
 
 
-TABLE1 .text "INTRODUISEZ VOS DISQUETTES S.V.P:"
- .byte 0
-TABLE2 .text "APPLE BITCOPIER (C) 1982         "
- .text 'JALB '
- .text " "
- .byte 0
+TABLE1      .text "INTRODUISEZ VOS DISQUETTES S.V.P:"
+            .byte 0
+TABLE2      .text "APPLE BITCOPIER (C) 1982         "
+            .text 'JALB '
+            .text " "
+            .byte 0
 
-TIRET LDX #$28
- LDA #$BD
-TIR1 JSR COUT
- DEX
- BNE TIR1
- RTS
+TIRET       ldx #$28
+            lda #$BD
+TIR1        jsr COUT
+            dex
+            bne TIR1
+            rts
 
 
 ;***********************
@@ -57,33 +57,33 @@ TIR1 JSR COUT
 ;                      *
 ;***********************
 
-START JSR HOME
- LDX #$00
- STA CH
- LDA #$06
- STA CH+1
- JSR VTAB
- JSR TIRET
- LDX #$00
-ST1 LDA TABLE2,X
- BEQ ST2
- JSR COUT
- INX
- BNE ST1
-ST2 JSR CROUT
- JSR TIRET
- LDA #$04
- STA $24
- LDA #$0C
- STA $25
- JSR VTAB
- LDX #$00
-ST3 LDA TABLE1,X
- BEQ ST4
- JSR COUT
- INX
- BNE ST3
-ST4 JSR RDKEY
+START       jsr HOME
+            ldx #$00
+            sta CH
+            lda #$06
+            sta CH+1
+            jsr VTAB
+            jsr TIRET
+            ldx #$00
+ST1         lda TABLE2,X
+            beq ST2
+            jsr COUT
+            inx
+            bne ST1
+ST2         jsr CROUT
+            jsr TIRET
+            lda #$04
+            sta $24
+            lda #$0C
+            sta $25
+            jsr VTAB
+            ldx #$00
+ST3         lda TABLE1,X
+            beq ST4
+            jsr COUT
+            inx
+            bne ST3
+ST4         jsr RDKEY
 
 ;***********************
 ;                      *
@@ -91,22 +91,22 @@ ST4 JSR RDKEY
 ;                      *
 ;***********************
 
-INIT LDA #$00 ;PISTE DEPART
- STA $02
- LDA #$23 ;PISTE FIN
- STA $03
- LDA #$D5 ;MARQUEURS
- STA $F9
- LDA #$AA
- STA $05
- LDA #$96
- STA $F8
- LDA #$B5
- STA $F7
- JSR HOME
- JSR AFFTRK
- JSR PISTE0
- JMP PGM01
+INIT        lda #$00 ;PISTE DEPART
+            sta $02
+            lda #$23 ;PISTE FIN
+            sta $03
+            lda #$D5 ;MARQUEURS
+            sta $F9
+            lda #$AA
+            sta $05
+            lda #$96
+            sta $F8
+            lda #$B5
+            sta $F7
+            jsr HOME
+            jsr AFFTRK
+            jsr PISTE0
+            jmp PGM01
 
 ;***********************
 ;                      *
@@ -114,65 +114,65 @@ INIT LDA #$00 ;PISTE DEPART
 ;                      *
 ;***********************
 
-ECRIT LDA #$02
- JSR SEEK0
- LDA DRVON,X
- LDY #$05
-ECR1 LDA #$FF
- JSR WAIT
- DEY
- BNE ECR1
- LDY #$FF
- LDA OUTBYT,X
- LDA DRVCTL1,X
- LDA #$FF
- STA DRVCTL2,X
- ORA INBYT,X
- CLC
- CLC
- CLC
- CLC
- CLC
- LDA #$FF
- JSR WAIT12
-ECR2 PHA
- PLA
- STA OUTBYT,X
- ORA INBYT,X
- DEY
- BEQ ECR3
- JSR WAIT12
- LDA #$FF
- STX $60
- JMP ECR2
-ECR3 LDA TEST,Y
- CLC
- CLC
- INY
- PHA
- PLA
- JMP ECR5
-ECR4 LDA TEST,Y
+ECRIT       lda #$02
+            jsr SEEK0
+            lda DRVON,X
+            ldy #$05
+ECR1        lda #$FF
+            jsr WAIT
+            dey
+            bne ECR1
+            ldy #$FF
+            lda OUTBYT,X
+            lda DRVCTL1,X
+            lda #$FF
+            sta DRVCTL2,X
+            ora INBYT,X
+            clc
+            clc
+            clc
+            clc
+            clc
+            lda #$FF
+            jsr WAIT12
+ECR2        pha
+            pla
+            sta OUTBYT,X
+            ora INBYT,X
+            dey
+            beq ECR3
+            jsr WAIT12
+            lda #$FF
+            stx $60
+            jmp ECR2
+ECR3        lda TEST,Y
+            clc
+            clc
+            iny
+            pha
+            pla
+            jmp ECR5
+ECR4        lda TEST,Y
 PTR = *-1
- INY
- BEQ ECR7
- CMP #$00
- BEQ ECR8
- CLC
- CLC
- CLC
-ECR5 CLC
-ECR6 STA OUTBYT,X
- ORA INBYT,X
- JMP ECR4
-ECR7 INC PTR
- CLC
- JMP ECR6
-ECR8 LDA DRVCTL1,X
- LDA DRVOFF,X
- LDA #$70
- STA PTR
-WAIT12 RTS
+            iny
+            beq ECR7
+            cmp #$00
+            beq ECR8
+            clc
+            clc
+            clc
+ECR5        clc
+ECR6        sta OUTBYT,X
+            ora INBYT,X
+            jmp ECR4
+ECR7        inc PTR
+            clc
+            jmp ECR6
+ECR8        lda DRVCTL1,X
+            lda DRVOFF,X
+            lda #$70
+            sta PTR
+WAIT12      rts
 
 ;***********************
 ;                      *
@@ -180,27 +180,27 @@ WAIT12 RTS
 ;                      *
 ;***********************
 
-SEEK0 PHA
- JSR GETIOB
- STY $00
- STA $01
- PLA
- LDY #$02
- STA ($00),Y
- LDA $02
- LDY #$04
- STA ($00),Y
- LDA #$00
- LDY #$0C
- STA ($00),Y
- JSR GETIOB
- JSR RWTS
- LDA #$00
- STA $48
- LDY #$01
- LDA ($00),Y
- TAX
- RTS
+SEEK0       pha
+            jsr GETIOB
+            sty $00
+            sta $01
+            pla
+            ldy #$02
+            sta ($00),Y
+            lda $02
+            ldy #$04
+            sta ($00),Y
+            lda #$00
+            ldy #$0C
+            sta ($00),Y
+            jsr GETIOB
+            jsr RWTS
+            lda #$00
+            sta $48
+            ldy #$01
+            lda ($00),Y
+            tax
+            rts
 
 ;***********************
 ;                      *
@@ -208,27 +208,27 @@ SEEK0 PHA
 ;                      *
 ;***********************
 
-LECTURE LDA #$01
- JSR SEEK0
-LECT1 LDX #$60
- LDA DRVON,X
- LDA DRVCTL1,X
- LDA #$00
- STA $00
- LDA #$30
- STA $01
- LDY #$00
-LECT2 LDA INBYT,X
- BPL LECT2
- STA ($00),Y
- INC $00
- BNE LECT2
- INC $01
- LDA $01
- CMP #$6F
- BCC LECT2
- LDA DRVOFF,X
- RTS
+LECTURE     lda #$01
+            jsr SEEK0
+LECT1       ldx #$60
+            lda DRVON,X
+            lda DRVCTL1,X
+            lda #$00
+            sta $00
+            lda #$30
+            sta $01
+            ldy #$00
+LECT2       lda INBYT,X
+            bpl LECT2
+            sta ($00),Y
+            inc $00
+            bne LECT2
+            inc $01
+            lda $01
+            cmp #$6F
+            bcc LECT2
+            lda DRVOFF,X
+            rts
 
 ;***********************
 ;                      *
@@ -236,149 +236,149 @@ LECT2 LDA INBYT,X
 ;                      *
 ;***********************
 
-PGM01 LDA #'R'
- JSR AFFICH
- LDA #$00
- STA $FF
-PGM02 JSR LECTURE
- LDA #'A'
- JSR AFFICH
- JSR ANALYSE
- BCS PGM09
- LDY #$00
- STY $FC
- LDA #$70
- STA $FD
-PGM03 LDA ($FA),Y
- STA ($FC),Y
- INY
- BNE PGM03
- INC $FB
- INC $FD
- LDA $FD
- CMP #$90
- BNE PGM03
- LDA #$05
- STA $25
- LDA #$00
- STA $24
- STA $3C
- LDA #$70
- STA $3D
- STA $3F
- LDA #$5F
- STA $3E
- JSR XAM ;IMPRIME A1 ($3C,$3D)
- LDA #$70
- STA $FB
- LDY #$00
- STY $FA
- STY $FC
- LDA #$75
- STA $FD
- LDY #$00
- LDX #$00
- STX $FE
-PGM04 LDA ($FC),Y
- CMP TEST,X
- BEQ PGM06
-PGM05 INY
- BNE PGM04
- INC $FD
- INC $FE
- LDA $FE
- CMP #$20
- BNE PGM04
- JMP PGM13
-PGM06 TYA
- PHA
- LDA $FD
- PHA
-PGM07 INX
- CPX #$0A
- BEQ PGM15
- INY
- BNE PGM08
- INC $FD
-PGM08 LDA ($FC),Y
- CMP TEST,X
- BEQ PGM07
- PLA
- STA $FD
- PLA
- TAY
- LDX #$00
- JMP PGM05
-PGM09 LDA $FF
- CMP #$04
- BEQ PGM10
- INC $FF
- LDA #'R'
- JSR AFFICH
- JMP PGM02
-PGM10 LDA #'1'
- JSR AFFICH
- JMP PGM21
-PGM11 LDA $FF
- CMP #$04
- BEQ PGM12
- INC $FF
- JMP PGM16
-PGM12 LDA #'2'
- JSR AFFICH
- JMP PGM21
-PGM13 LDA $FF
- CMP #$04
- BEQ PGM14
- INC $FF
- LDA #'R'
- JSR AFFICH
- JMP PGM02
-PGM14 LDA #'3'
- JSR AFFICH
- JMP PGM21
-PGM15 PLA
- STA $FD
- PLA
- TAY
- LDA #$00
- STA ($FC),Y
-PGM16 LDA #'W'
- JSR AFFICH
- JSR ECRIT
- LDA #'V'
- JSR AFFICH
- JSR LECT1
- LDA #'A'
- JSR AFFICH
- LDA #$30
- STA $FD
- LDY #$00
- STA $FC
- LDX #$00
-PGM17 LDA ($FC),Y
- CMP TEST,X
- BNE PGM18
- INX
- CPX #$10
- BEQ PGM20
- BNE PGM19
-PGM18 LDX #$00
-PGM19 INY
- BNE PGM17
- INC $FD
- LDA $FD
- CMP #$4F
- BNE PGM17
- JMP PGM11
-PGM20 LDA #$B0
- JSR AFFICH
-PGM21 LDA $02
- CMP $03
- BEQ PGM22
- INC $02
- JMP PGM01
-PGM22 BRK
+PGM01       lda #'R'
+            jsr AFFICH
+            lda #$00
+            sta $FF
+PGM02       jsr LECTURE
+            lda #'A'
+            jsr AFFICH
+            jsr ANALYSE
+            bcs PGM09
+            ldy #$00
+            sty $FC
+            lda #$70
+            sta $FD
+PGM03       lda ($FA),Y
+            sta ($FC),Y
+            iny
+            bne PGM03
+            inc $FB
+            inc $FD
+            lda $FD
+            cmp #$90
+            bne PGM03
+            lda #$05
+            sta $25
+            lda #$00
+            sta $24
+            sta $3C
+            lda #$70
+            sta $3D
+            sta $3F
+            lda #$5F
+            sta $3E
+            jsr XAM ;IMPRIME A1 ($3C,$3D)
+            lda #$70
+            sta $FB
+            ldy #$00
+            sty $FA
+            sty $FC
+            lda #$75
+            sta $FD
+            ldy #$00
+            ldx #$00
+            stx $FE
+PGM04       lda ($FC),Y
+            cmp TEST,X
+            beq PGM06
+PGM05       iny
+            bne PGM04
+            inc $FD
+            inc $FE
+            lda $FE
+            cmp #$20
+            bne PGM04
+            jmp PGM13
+PGM06       tya
+            pha
+            lda $FD
+            pha
+PGM07       inx
+            cpx #$0A
+            beq PGM15
+            iny
+            bne PGM08
+            inc $FD
+PGM08       lda ($FC),Y
+            cmp TEST,X
+            beq PGM07
+            pla
+            sta $FD
+            pla
+            tay
+            ldx #$00
+            jmp PGM05
+PGM09       lda $FF
+            cmp #$04
+            beq PGM10
+            inc $FF
+            lda #'R'
+            jsr AFFICH
+            jmp PGM02
+PGM10       lda #'1'
+            jsr AFFICH
+            jmp PGM21
+PGM11       lda $FF
+            cmp #$04
+            beq PGM12
+            inc $FF
+            jmp PGM16
+PGM12       lda #'2'
+            jsr AFFICH
+            jmp PGM21
+PGM13       lda $FF
+            cmp #$04
+            beq PGM14
+            inc $FF
+            lda #'R'
+            jsr AFFICH
+            jmp PGM02
+PGM14       lda #'3'
+            jsr AFFICH
+            jmp PGM21
+PGM15       pla
+            sta $FD
+            pla
+            tay
+            lda #$00
+            sta ($FC),Y
+PGM16       lda #'W'
+            jsr AFFICH
+            jsr ECRIT
+            lda #'V'
+            jsr AFFICH
+            jsr LECT1
+            lda #'A'
+            jsr AFFICH
+            lda #$30
+            sta $FD
+            ldy #$00
+            sta $FC
+            ldx #$00
+PGM17       lda ($FC),Y
+            cmp TEST,X
+            bne PGM18
+            inx
+            cpx #$10
+            beq PGM20
+            bne PGM19
+PGM18       ldx #$00
+PGM19       iny
+            bne PGM17
+            inc $FD
+            lda $FD
+            cmp #$4F
+            bne PGM17
+            jmp PGM11
+PGM20       lda #$B0
+            jsr AFFICH
+PGM21       lda $02
+            cmp $03
+            beq PGM22
+            inc $02
+            jmp PGM01
+PGM22       brk
 
 ;***********************
 ;                      *
@@ -386,100 +386,100 @@ PGM22 BRK
 ;                      *
 ;***********************
 
-ANALYSE LDA #$00
- TAY
- STA $FA
- LDA #$30
- STA $FB
-ANA01 LDA ($FA),Y
- CMP $F9
- BEQ ANA03
-ANA02 INY
- BNE ANA01
- INC $FB
- LDA $FB
- CMP #$4F
- BNE ANA01
- JMP ANA08
-ANA03 TYA
- PHA
- LDA $FB
- PHA
- INY
- BNE ANA04
- INC $FB
-ANA04 LDA ($FA),Y
- CMP $05
- BNE ANA07
- INY
- BNE ANA05
- INC $FB
-ANA05 LDA ($FA),Y
- CMP $F8
- BEQ ANA06
- CMP $F7
- BNE ANA07
-ANA06 PLA
- STA $FB
- PLA
- TAY
- CLC
- ADC $FA
- STA $FA
- LDA #$00
- ADC $FB
- STA $FB
- CLC
- RTS
-ANA07 PLA
- STA $FB
- PLA
- TAY
- JMP ANA02
-ANA08 LDA #$00
- TAY
- STA $FA
- LDA #$30
- STA $FB
-ANA09 JSR SYNCR
- BCS ANA12
- LDA ($FA),Y
- CMP $F9
- BNE ANA09
- LDA $FB
- PHA
- INY
- BNE ANA10
- INC $FB
-ANA10 LDA ($FA),Y
- CMP $05
- BNE ANA11
- PLA
- STA $FB
- CLC
- RTS
-ANA11 PLA
- STA $FB
- JMP ANA09
-ANA12 LDA #$00
- TAY
- STA $FA
- LDA #$30
- STA $FB
-ANA13 JSR SYNCR
- BCS ANA14
- LDA ($FA),Y
- CMP $F9
- BNE ANA13
- CLC
- RTS
-ANA14 LDA #$00
- TAY
- STA $FA
- LDA #$30
- STA $FB
- JSR SYNCR
- RTS
+ANALYSE     lda #$00
+            tay
+            sta $FA
+            lda #$30
+            sta $FB
+ANA01       lda ($FA),Y
+            cmp $F9
+            beq ANA03
+ANA02       iny
+            bne ANA01
+            inc $FB
+            lda $FB
+            cmp #$4F
+            bne ANA01
+            jmp ANA08
+ANA03       tya
+            pha
+            lda $FB
+            pha
+            iny
+            bne ANA04
+            inc $FB
+ANA04       lda ($FA),Y
+            cmp $05
+            bne ANA07
+            iny
+            bne ANA05
+            inc $FB
+ANA05       lda ($FA),Y
+            cmp $F8
+            beq ANA06
+            cmp $F7
+            bne ANA07
+ANA06       pla
+            sta $FB
+            pla
+            tay
+            clc
+            adc $FA
+            sta $FA
+            lda #$00
+            adc $FB
+            sta $FB
+            clc
+            rts
+ANA07       pla
+            sta $FB
+            pla
+            tay
+            jmp ANA02
+ANA08       lda #$00
+            tay
+            sta $FA
+            lda #$30
+            sta $FB
+ANA09       jsr SYNCR
+            bcs ANA12
+            lda ($FA),Y
+            cmp $F9
+            bne ANA09
+            lda $FB
+            pha
+            iny
+            bne ANA10
+            inc $FB
+ANA10       lda ($FA),Y
+            cmp $05
+            bne ANA11
+            pla
+            sta $FB
+            clc
+            rts
+ANA11       pla
+            sta $FB
+            jmp ANA09
+ANA12       lda #$00
+            tay
+            sta $FA
+            lda #$30
+            sta $FB
+ANA13       jsr SYNCR
+            bcs ANA14
+            lda ($FA),Y
+            cmp $F9
+            bne ANA13
+            clc
+            rts
+ANA14       lda #$00
+            tay
+            sta $FA
+            lda #$30
+            sta $FB
+            jsr SYNCR
+            rts
 
 ;***********************
 ;                      *
@@ -487,43 +487,43 @@ ANA14 LDA #$00
 ;                      *
 ;***********************
 
-SYNCR LDX #$00
-SYN01 LDA ($FA),Y
- CMP #$FF
- BNE SYN03
- INX
- CPX #$05
- BEQ SYN05
-SYN02 INY
- BNE SYN01
- INC $FB
- LDA $FB
- CMP #$4F
- BEQ SYN04
- BNE SYN01
-SYN03 LDX #$00
- JMP SYN02
-SYN04 SEC
- RTS
-SYN05 INY
- BNE SYN06
- INC $FB
- LDA $FB
- CMP #$4F
- BEQ SYN04
-SYN06 LDA ($FA),Y
- CMP #$FF
- BEQ SYN05
- CLC
- TYA
- ADC $FA
- STA $FA
- LDA #$00
- ADC $FB
- STA $FB
- LDY #$00
- CLC
- RTS
+SYNCR       ldx #$00
+SYN01       lda ($FA),Y
+            cmp #$FF
+            bne SYN03
+            inx
+            cpx #$05
+            beq SYN05
+SYN02       iny
+            bne SYN01
+            inc $FB
+            lda $FB
+            cmp #$4F
+            beq SYN04
+            bne SYN01
+SYN03       ldx #$00
+            jmp SYN02
+SYN04       sec
+            rts
+SYN05       iny
+            bne SYN06
+            inc $FB
+            lda $FB
+            cmp #$4F
+            beq SYN04
+SYN06       lda ($FA),Y
+            cmp #$FF
+            beq SYN05
+            clc
+            tya
+            adc $FA
+            sta $FA
+            lda #$00
+            adc $FB
+            sta $FB
+            ldy #$00
+            clc
+            rts
 
 ;***********************
 ;                      *
@@ -531,17 +531,17 @@ SYN06 LDA ($FA),Y
 ;                      *
 ;***********************
 
-AFFICH LDX $02
- STA SCREEN,X
- RTS
-AFFTRK LDX #$00
-AFF1 LDA TEXTE,X
- CMP #$00
- BEQ AFFRTS
- JSR COUT
- INX
- JMP AFF1
-AFFRTS RTS
+AFFICH      ldx $02
+            sta SCREEN,X
+            rts
+AFFTRK      ldx #$00
+AFF1        lda TEXTE,X
+            cmp #$00
+            beq AFFRTS
+            jsr COUT
+            inx
+            jmp AFF1
+AFFRTS      rts
 
 ;***********************
 ;                      *
@@ -549,17 +549,17 @@ AFFRTS RTS
 ;                      *
 ;***********************
 
-PISTE0 LDA $02
- PHA
- LDA #$FF
- STA $02
- LDA #$01
- JSR SEEK0
- LDA #$02
- JSR SEEK0
- PLA
- STA $02
- RTS
+PISTE0      lda $02
+            pha
+            lda #$FF
+            sta $02
+            lda #$01
+            jsr SEEK0
+            lda #$02
+            jsr SEEK0
+            pla
+            sta $02
+            rts
 
 ;***********************
 ;                      *
@@ -568,14 +568,14 @@ PISTE0 LDA $02
 ;***********************
 
 TEXTE
- .text "TR 000000000000000011111111111111112222"
- .byte $8D
- .text "AC 0123456789ABCDEF0123456789ABCDEF0123"
- .byte $8D
- .text "---------------------------------------"
- .byte $8D
- .text "ER"
- .byte $8D
- .text "---------------------------------------"
- .byte $8D
- .byte 0
+            .text "TR 000000000000000011111111111111112222"
+            .byte $8D
+            .text "AC 0123456789ABCDEF0123456789ABCDEF0123"
+            .byte $8D
+            .text "---------------------------------------"
+            .byte $8D
+            .text "ER"
+            .byte $8D
+            .text "---------------------------------------"
+            .byte $8D
+            .byte 0
