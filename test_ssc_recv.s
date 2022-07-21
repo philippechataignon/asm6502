1ea5ec404081057dc0288ba35a83bf1e3c0d6af6
Philippe Chataignon <philippe@ldlc>
2022-07-21 14:24:31 +0200

    Add test_ssc_recv.s

---
+++ test_ssc_recv.s
@@ -0,0 +1,60 @@
+DIRECT := false
+
+automod = $1234
+
+; XMODEM Control Character Constants
+SOH = $01                ; start block
+EOT = $04                ; end of text marker
+ACK = $06                ; good block acknowledged
+NAK = $15                ; bad block acknowledged
+
+home = $fc58
+endofline1 = $427
+
+ssc.exitkbd := exit
+
+.include "apple_enc.inc"
+.enc "none"
+
+.include "macros.inc"
+
+*       =  $803
+
+safe_getc       .macro
+                jsr ssc.getc3s
+                bcc exit        ; if timeout, exit
+                sta endofline1
+                .endm
+
+entry
+                jsr home
+                jsr ssc.init
+                jsr ssc.flush
+                lda #NAK
+                jsr ssc.putc
+
+                ldy #0
+-               safe_getc
+                sta $1000,y
+                iny
+                cpy #132
+                bne -
+
+                lda #ACK
+                jsr ssc.putc
+
+                ldy #0
+-               safe_getc
+                sta $1100,y
+                iny
+                cpy #132
+                bne -
+exit            rts
+
+ssc             .binclude "ssc.s"
+
+; print subroutine
+printstr        .binclude "printstr.s"
+
+                .enc "apple"
+msg_norecv      .null "NO RECV\n"
