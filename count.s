c6359b0d984867ebaa6a2b6d47bc5199fa8563bf
Philippe Chataignon <philippe@inspiron>
2023-02-24 11:30:49 +0100

    16 bits counter

--- count.s
+++ count.s
@@ -13,17 +13,25 @@ count   = $1100
         ldy #0
         lda #0
 -       sta (ptr),y
+        inc ptr+1   ; page+1
+        sta (ptr),y
+        dec ptr+1
         iny
         bne -
 
 ; count each #byte
 ; starts at 1 because max # is 255
-        ldx #1
+        ldx #0
 loop    ldy data,x
         lda (ptr),y
         clc
         adc #1
         sta (ptr),y
+        inc ptr+1   ; page+1
+        lda (ptr),y
+        adc #0
+        sta (ptr),y
+        dec ptr+1   ; restore page
         inx
         bne loop
 exit    rts
