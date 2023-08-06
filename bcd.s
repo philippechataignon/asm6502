b7e0b010a2b97bb10961a36770142cf38277134d
Philippe Chataignon <philippe@inspiron>
2023-08-06 12:47:03 +0200

    Unify 16/32 bits for bcd

--- bcd.s
+++ bcd.s
@@ -1,6 +1,6 @@
-; Convert an 32 bit binary value to BCD
+; Convert an 16/32 bit binary value to BCD
 ;
-; This function converts a 32 bit binary value into a 40 bit BCD. It
+; This function converts a 16/32 bit binary value into a 24/40 bit BCD. It
 ; works by transferring one bit a time from the source and adding it
 ; into a BCD value that is being doubled on each iteration. As all the
 ; arithmetic is being done in BCD the result is a binary to decimal
@@ -8,22 +8,26 @@
 
 ; 1234567890 -> BCD: $12 $34 $56 $78 $90
 
+BITS := 32
+
 PRBYTE      = $FDDA
 PRHEX       = $FDE3
 COUT        = $FDED
 
-BCD_POS     = 5
 F0ADDR      = $EB
 BIN         = $EC
-BCD         = BIN + 4
+BCD         = BIN + BITS / 8
+BCD_POS     = BITS / 8 + 1
 
 .include "apple_enc.inc"
 .enc "apple"
 
+.if DIRECT
 * = $300
+.fi
 
-BINBCD32:
-        lda #$F0
+BINBCD:
+        lda #$F0        ; bit instr has no immediate mode
         sta F0ADDR
         sed             ; Switch to decimal mode
         ldx #BCD_POS
@@ -32,12 +36,13 @@ BINBCD32:
         dex
         bne -
 
-        ldy #32         ; The number of source bits
+        ldy #BITS       ; The number of source bits
 CNVBIT: asl BIN         ; Shift out one bit
         rol BIN+1
+.if BITS == 32
         rol BIN+2
         rol BIN+3
-
+.endif
         ldx #BCD_POS
 -       lda BCD-1,x     ; And add into result
         adc BCD-1,x     ; propagating any carry
@@ -60,11 +65,11 @@ NIBBLE:
         bit F0ADDR      ; "and" #$F0
         bne PRINT       ; si XY, normal print
         jsr PRHEX       ; else 0Y, PRHEX
-        jmp +           ; next bxte
+        jmp +           ; next byte
 PRINT:
 -       lda BCD,x
         jsr PRBYTE
 +       inx
         cpx #BCD_POS
         blt -
-END     rts
+        rts
