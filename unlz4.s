daeb7c46bf6ee9179fbe89f070c527a8fdd90024
Philippe Chataignon <philippe.chataignon@gmail.com>
2024-08-08 12:09:10 +0200

    Add lz4 decompress example

--- unlz4.s
+++ unlz4.s
@@ -1,5 +1,40 @@
 ;LZ4 data decompressor for Apple II
 ;
+; http://fastcompression.blogspot.com/2011/05/lz4-explained.html
+
+;   token   | literal length | literals  | offset  | match length
+;  1 byte   | 0-n bytes      | 0-L bytes | 2 bytes | 0-n bytes
+
+; token aaaabbbb
+; aaaa = length of literals 0 = no literal, $f need more bytes
+; bbbb = match length 0 = 4 $f = 19 after offset
+
+; Example
+; 00000000: *b4*56 6f69 7475   7265 2075 6e0a 0b00 *45*64  .Voiture un...Ed
+; 00000010: 6575   780d 00*55* 7472 6f69 730e 0070 *71*75  eux..Utrois..pqu
+; 00000020: 6174   7265 0a                                 atre.
+
+; token b4: b = 11 = length of literals
+; output Voiture un@
+;        01234567890
+; offset 000b -> -11 before offset, match = 4 + 4 = 8 -> Voiture(space)
+; output Voiture un@Voiture
+;        0123456789012345678
+; token 45: 4 literals = deux
+; output Voiture un@Voiture deux
+;        01234567890123456789012
+; offset 000d -> -13 (@ position), match length = 5+4 = 9
+; output Voiture un@Voiture deux@Voiture
+;        01234567890123456789012345678901
+; token 55
+; output Voiture un@Voiture deux@Voiture trois
+;        0123456789012345678901234567890123456
+; offset 000e, length 5+4=9
+; output Voiture un@Voiture deux@Voiture trois@Voiture
+;        0123456789012345678901234567890123456789012345
+; token 71
+; output Voiture un@Voiture deux@Voiture trois@Voiture quatre@
+;        0123456789012345678901234567890123456789012345
 
 src = $06
 dst = $08
@@ -19,7 +54,7 @@ parsetoken
         lsr
         lsr
         beq copymatches ; if 0, no litterals
-        jsr buildcount  ; A = LSB #, count = MSB #
+        jsr buildcount  ; after, A = LSB #, count = MSB #
         tax
         jsr docopy      ; use X LSB / count MSB
         lda src         ; src >= end ?
@@ -59,7 +94,7 @@ copymatches             ; else copymatch phase
         sta src
         jmp parsetoken  ; end of block -> next token
 
-docopy  jsr getput      ; copy X + 256 * [count] litterals/
+docopy  jsr getput      ; copy X + 256 * [count] litterals
         dex             ; get value
         bne docopy
         dec count
@@ -69,9 +104,9 @@ docopy  jsr getput      ; copy X + 256 * [count] litterals/
 ; example1: 4x
 ; at enter, A = $04, at exit A = 4 and count = 0
 ;
-; example2: Fx FF F3 for #litterals
+; example2: Fx FF F3 for literals length
 ; at enter, A = $0F
-; tmp only matters to get C and increment count
+; tmp only matters to get carry C and increment count
 ; count = 0, then get next A = $FF
 ; $FF + $F = $10E > $100 -> count = 1, A = $0E
 ; count = 1, tmp = $0E then get next A = $F3
@@ -85,7 +120,7 @@ buildcount              ; build count from token nibble
         bne bc_rts      ; else exit
 -       sta tmp         ; store A=LSB in tmp
         jsr getsrc      ; get next byte in A
-        tax             ; then inx,bne to test ==$FF
+        tax             ; copy in X for $FF test
         clc             ; tmp ~ count LSB
         adc tmp         ; if A + tmp > $FF, C is set
         bcc +           ;
@@ -97,14 +132,17 @@ bc_rts  rts
 lzdone  pla             ; restore stack (parsetoken pha)
         rts
 
+; get byte from src and put to dest
 getput  jsr getsrc      ; (dst++) <- (src++)
 
+; put byte from A to dest
 putdst  sta (dst), y    ; store to (dst++)
         inc dst
         bne +
         inc dst+1
 +       rts
 
+; get byte in A from src
 getsrc  lda (src), y    ; get from (src++)
         inc src
         bne +
