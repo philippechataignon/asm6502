506de42d319523b81cf75923849e8ad4a51e3151
Philippe Chataignon <philippe.chataignon@gmail.com>
2024-08-09 09:38:45 +0200

    Move end test

--- unlz4b.s
+++ unlz4b.s
@@ -18,7 +18,6 @@
 unlz4
 ;literal length
                 jsr    get_byte         ; get token
-                php                     ; push P to test C if end reaches
                 sta    token            ; store for future use (match length)
                 lsr    a                ; get high nibble
                 lsr    a
@@ -31,40 +30,43 @@ unlz4
 literals        jsr    get_byte         ; read byte
                 jsr    store_byte       ; and store
                 bne    literals         ; until len
-                plp                     ; restore P
+                lda   src               ; src >= end ?
+                cmp   end
+                lda   src+1
+                sbc   end+1             ; carry set if start >= end
                 bcc    read_offset      ; if C clear, continue
                 rts                     ; unlz4 exit
 ; get offset and calc source address
 read_offset     jsr    get_byte         ; get LSB offset
-                sec                     ; put (dest - offset) in src
-                eor    #$ff             ; eor 1 = FE = -2 = -1 - 1
+                sec                     ; put (dest - offset) in tmpsrc
+                eor    #$ff             ;
                 adc    dest             ;
-                sta    src              ; idem with MSB offset
-                jsr    get_byte
+                sta    tmpsrc
+                jsr    get_byte         ; idem with MSB offset
                 eor    #$ff
                 adc    dest+1
-                sta    src+1
+                sta    tmpsrc+1
 
 ; calc match length from saved token
 +               lda    #$ff             ; get token
-token           =      *-1
+token           =      * - 1
                 and    #$0f             ; get low nibble
                 adc    #$03             ; add 4 (C always set ?)
                 cmp    #$13             ; equivalent to cmp $0f
                 jsr    getLength
 ; copy matches loop
 -               lda    $1234
-src             =      *-2
-                inc    src
+tmpsrc          =      * - 2
+                inc    tmpsrc
                 bne    +
-                inc    src+1
+                inc    tmpsrc+1
 +               jsr    store_byte
                 bne    -
                 beq    unlz4            ; next byte (token)
 
 ; -- store byte and decr len
 store_byte      sta    $1234
-dest            =      *-2
+dest            =      * - 2
                 inc    dest
                 bne    +
                 inc    dest+1
@@ -73,31 +75,27 @@ dest            =      *-2
                 dec    lenh
 +               rts
 
-; -- get byte and incr source
+; -- get byte and incr src
 get_byte        lda   $1234
-source          =     *-2
-                inc   source
+src             =     * - 2
+                inc   src
 			    bne   +
-			    inc   source+1
-                lda   source                ; src >= end ?
-                cmp   end
-                lda   source+1
-                sbc   end+1                 ; carry set if start >= end
+			    inc   src+1
 +               rts
 
 ; -- calc length, store in lenHL
 -               jsr    get_byte
                 tay
                 clc
-                adc    #$00                 ; $00 replaced by LSB length
+                adc    #$00             ; $00 replaced by LSB length
 lenl            =      *-1
-                bcc    +                    ; 16 bits length increment
+                bcc    +                ; 16 bits length increment
                 inc    lenh
-+               iny                         ; test if $ff with beq
-getLength       sta    lenl                 ; do not modify flags, uses in adc #
-                beq    -                    ; Z from "cmp $0f" when initial call
-                                            ; else from "iny"
-                tay                         ;
++               iny                     ; test if $ff with beq
+getLength       sta    lenl             ; do not modify flags, uses in adc #
+                beq    -                ; Z from "cmp $0f" when initial call
+                                        ; else from "iny"
+                tay                     ;
                 beq    +
                 inc    lenh
 +               rts
