300658107f6bd4339cf689eb70ccd943059042ed
Philippe Chataignon <philippe.chataignon@gmail.com>
2024-10-26 17:24:16 +0200

    Use indirect jump in msgout

--- msgout.s
+++ msgout.s
@@ -1,21 +1,20 @@
-
 msgout
 _COUT = $FDED
-            pla                     ; get calling addr in TEMP
-            sta _PTR
+            pla                     ; get calling addr
+            sta _PTR                ; in _PTR
             pla
             sta _PTR+1
-            ldy #0                  ; always 0
+            ldy #0
 -           inc _PTR
             bne +
             inc _PTR+1
-+           lda $1234,y
++           lda $1234,y             ; load char
 _PTR = * -2
-            beq _exit
-            jsr _COUT
-            jmp -
-_exit       lda _PTR+1              ; TEMP points on next instr
-            pha
-            lda _PTR
-            pha
-            rts                     ; use TEMP as return addr
+            beq _exit               ; if NULL, exit
+            jsr _COUT               ; display
+            jmp -                   ; and next
+_exit
+            inc _PTR                ; increment return addr
+            bne +
+            inc _PTR+1
++           jmp (_PTR)              ; and jump
